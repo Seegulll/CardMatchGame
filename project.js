6733bda22e069939c903cae1a55d3a65b0163eae
Vania Sameen <128991354+vaniaSam@users.noreply.github.com>
2024-03-22 19:10:40 -0400

    Update project.js

--- project.js
+++ project.js
@@ -7,6 +7,7 @@ function singlePlayer() {
 
 // create an update Timer function so that it can be passed through setInterval()
 var countdown = 60;
+var timerInterval;
 function updateTimer() {
     countdown--;
     var timerId = document.getElementById('-id-of-timer-in-html');
@@ -23,7 +24,7 @@ function timer() {
     // Call the updateTimer function immediately to update the timer initially. if we dont add this
     // the timer won't display the initial countdown value until 1 second after the page loads.
     updateTimer();
-    var timerInterval = setInterval(updateTimer, 1000);
+    timerInterval = setInterval(updateTimer, 1000);
 }
 
 function timerEnd() {
