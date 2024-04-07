a9d1ac7b670bd1f54eb3fe69d9570866b55a221d
Seegulll <61858728+Seegulll@users.noreply.github.com>
2024-04-07 11:01:55 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,43 @@
+
+
+function modeSelect() 
+	{setTimeout(function (){
+		document.getElementById("button3").style.visibility = "visible";
+		document.getElementById("button3").onclick = startPage;
+		document.getElementById("button1").innerHTML = "Single Player";
+		document.getElementById("button1").onclick = singlePlayerPage;
+		document.getElementById("button2").innerHTML = "Multiplayer";
+		document.getElementById("button2").onclick = startMultiPlayer;
+	}, 20);
+}
+
+function singlePlayerPage(){
+	document.getElementById("button3").style.visibility = "visible";
+	document.getElementById("button3").onclick = modeSelect;
+	document.getElementById("button1").innerHTML = "Play";
+	document.getElementById("button1").onclick = startSinglePlayer;
+	document.getElementById("button2").innerHTML = "High Scores";
+	document.getElementById("button2").onclick = startMultiPlayer;
+}
+
+function startSinglePlayer(){
+}
+
+function startMultiplayer(){
+}
+
+function showHighScores(){
+	
+}
+
+function instructions(){
+	window.location.replace('project.html');
+}
+
+function startPage(){
+	document.getElementById("button1").innerHTML = "Start";
+	document.getElementById("button1").onclick = modeSelect;
+	document.getElementById("button2").innerHTML = "Instructions";
+	document.getElementById("button2").onclick = instructions;
+	document.getElementById("button3").style.visibility = "hidden";
+}
